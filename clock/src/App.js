3d139ccdd4a60e3002acfe9e5e0a8ecea2bad101
haoyue gao <gao115@purdue.edu>
2019-10-24 18:08:52 -0700

    add reset

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,65 +1,78 @@
 /* Class Component - Main Container */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          timeLeft: 0,
-          hour: 0,
-          min: 0,
-          sec: 0,
-          start: false
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            timeLeft: '',
+            hour: '',
+            min: '',
+            sec: '',
+            start: false,
+            intervalHandle: null
+        };
+    }
 
-  handleSubmit = (e) => {
-      e.preventDefault();
-      const { hour, min, sec } = this.state;
-      this.setState({
-          timeLeft: hour * 3600 + min * 60 + sec,
-          start: true
-      })
-      this.timer = setInterval(() => {
-          this.setState({
-              timeLeft: this.state.timeLeft - 1
-          })
-      }, 1000)
-  }
+    handleReset = () => {
+        const { intervalHandle } = this.state;
+        this.setState({ timeLeft: '', hour: '', min: '', sec: '', start: false });
+        clearInterval(intervalHandle);
+    }
 
-  format = (timeLeft) => {
-      let hour = Math.floor(timeLeft / 3600);
-      let min = Math.floor((timeLeft - hour * 3600) / 60);
-      let sec = timeLeft - min * 60 - hour * 3600;
-      return [hour, min, sec];
-  }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { timeLeft, hour, min, sec } = this.state;
+        if (hour === '' || min === '' || sec === '') {
+            alert('Please enter number');
+        } else {
+            this.setState({
+                timeLeft: hour * 3600 + min * 60 + sec,
+                start: true
+            })
+            let timer = setInterval(() => {
+                this.setState({
+                    timeLeft: this.state.timeLeft - 1
+                })
+            }, 1000)
+            this.setState({intervalHandle: timer});
+        }
+    }
 
-  render() {
-    const {start} = this.state;
-      return (
-          <div>
-              <div>
-                  <form onSubmit={this.handleSubmit}>
-                      <label>Hour:</label>
-                      <input type="number" onChange={(e) => { this.setState({ hour: Number(e.target.value) }) }} />
-                      <label>Miniute:</label>
-                      <input type="number" onChange={(e) => this.setState({ min: Number(e.target.value) })} />
-                      <label>Second:</label>
-                      <input type="number" onChange={(e) => this.setState({ sec: Number(e.target.value) })} />
-                      <br />
-                      <input type="submit" value="Submit" />
-                  </form>
-              </div>
-              <div>{start && <Display timeLeft={this.state.timeLeft} format={this.format} />}</div>
-              {/* <div>
+    format = (timeLeft) => {
+        let hour = Math.floor(timeLeft / 3600);
+        let min = Math.floor((timeLeft - hour * 3600) / 60);
+        let sec = timeLeft - min * 60 - hour * 3600;
+        return [hour, min, sec];
+    }
+
+    render() {
+        const { start } = this.state;
+        return (
+            <div>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Hour:</label>
+                        <input type="number" value={this.state.hour} onChange={(e) => { this.setState({ hour: Number(e.target.value) }) }} />
+                        <label>Miniute:</label>
+                        <input type="number" value={this.state.min} onChange={(e) => this.setState({ min: Number(e.target.value) })} />
+                        <label>Second:</label>
+                        <input type="number" value={this.state.sec} onChange={(e) => this.setState({ sec: Number(e.target.value) })} />
+                        <br />
+                        <input type="submit" value="Submit" />
+                    </form>
+                    <button type='reset' onClick={this.handleReset}>Reset</button>
+                </div>
+                <div>{start && <Display timeLeft={this.state.timeLeft} format={this.format} />}</div>
+                {/* <div>
                   <label>{this.format(this.state.timeLeft)[0]}</label> <br />
                   <label>{this.format(this.state.timeLeft)[1]}</label> <br />
                   <label>{this.format(this.state.timeLeft)[2]}</label>
               </div> */}
 
-          </div>
-      )
-  }
+            </div>
+        )
+    }
 }
 
 export default App;
